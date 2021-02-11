35e6192b00c80064b1b71b4a9fb4a1f7da6e2e3d
Bernardo Morcon <bernardo.morcon@gmail.com>
2021-02-10 23:37:43 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middleware
+
+// routes
+app.use(require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// listening to the server
+app.listen(app.get('port'), () => {
+    console.log('Puerto:', app.get('port'));
+});
